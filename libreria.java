2b6634f604325eddb98191a960ef7b60c6f250f9
Oropeza56 <54290376+Oropeza56@users.noreply.github.com>
2020-10-30 21:00:38 -0600

    Create libreria.java

---
+++ libreria.java
@@ -0,0 +1,28 @@
+public class Libreria{
+	private String nombreLibro;
+	private String id;
+	private int numDeEjemp;
+	private String seccion;
+
+	public String getNombreLibro (){
+		return nombreLibro;
+	}
+
+	public String getId(){
+		return id;
+	}
+
+	public int getnumDeEjemp(){
+		return numDeEjemp;
+	}
+
+	public Libreria(String nombreLibro, String id, int numDeEjemp){
+		super ();
+		this.nombreLibro = nombreLibro;
+		this.id = id;
+		this numDeEjemp= numDeEjemp;
+	}
+
+
+	ArrayList <Libreria> loteLibros= new ArrayList<Libreria>();
+}
